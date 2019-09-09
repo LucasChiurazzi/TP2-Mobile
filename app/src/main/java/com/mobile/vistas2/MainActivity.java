79ff39ea1c1061f4a4bed2af0f65080647e09123
LucasChiurazzi <lucaschiurazzi@gmail.com>
2019-09-09 00:28:23 -0300

    fix1

--- app/src/main/java/com/mobile/vistas2/MainActivity.java
+++ app/src/main/java/com/mobile/vistas2/MainActivity.java
@@ -3,6 +3,7 @@ package com.mobile.vistas2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Checkable;
 import android.widget.EditText;
@@ -17,20 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.principal);
     }
 
-    public void convertir() {
+    public void convertir(View view) {
         EditText dolares = findViewById(R.id.eTD);
         EditText euros = findViewById(R.id.eTE);
         Checkable ead = findViewById(R.id.EaD);
         Checkable dae = findViewById(R.id.DaE);
+
         EditText result = findViewById(R.id.eTresultado);
         if (!dolares.getText().toString().isEmpty() || !euros.getText().toString().isEmpty()) {
             if (dae.isChecked()) {
                 Double d = Double.parseDouble(dolares.getText().toString());
                 String r = String.valueOf(d * DOLAR_EURO);
+
                 result.setText(r);
             }else{
                 Double e = Double.parseDouble(euros.getText().toString());
                 String r = String.valueOf(e * EURO_DOLAR);
+
                 result.setText(r);
             }
 
